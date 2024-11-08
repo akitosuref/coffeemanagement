70350deab3052e8a2cc00cc73ede97d97abf0977
akitosuref <vuongquangquy2511@gmail.com>
2024-11-08 19:10:16 +0700

    fix: Move module-info file to under java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,5 +10,6 @@ module com.group13.coffeemanagement {
     opens com.group13.coffeemanagement.database to com.google.gson;
     opens com.group13.coffeemanagement to com.google.gson;
     opens com.group13.coffeemanagement.controller to javafx.fxml;
+
     exports com.group13.coffeemanagement;
 }
